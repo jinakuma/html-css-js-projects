de09f5074931833c8aa654b6c18d6f9fecece617
jinakuma <utlumurat@gmail.com>
2021-11-21 11:20:16 +0300

    content_placeholder & navbar & drawing app & kinetic loader

---
+++ drawing_app/script.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const colorInput = document.getElementById('color');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeInput = document.getElementById('size');
+
+let size = sizeInput.innerText;
+let isPressed = false;
+let color;
+let x;
+let y;
+
+increaseBtn.addEventListener('click', () => {
+  size = sizeInput.innerText++;
+});
+
+decreaseBtn.addEventListener('click', () => {
+  size = sizeInput.innerText--;
+});
+
+colorInput.addEventListener('input', (e) => {
+  color = colorInput.value;
+});
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+    drawCircle(x2, y2);
+    drawLine(x, x2, y, y2);
+    x = x2;
+    y = y2;
+  }
+});
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, x2, y1, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+}
